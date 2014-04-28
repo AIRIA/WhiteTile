603a22d258f3a5af2225a2cd2eb727c5a184f47b
AIRIA <chaibingcheng0305@163.com>
2014-04-29 00:00:32 +0800

    change cpp format

---
+++ proj.android/src/com/giant/whitetile/AppRegister.java
@@ -0,0 +1,17 @@
+package com.giant.whitetile;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.tencent.mm.sdk.openapi.IWXAPI;
+import com.tencent.mm.sdk.openapi.WXAPIFactory;
+
+public class AppRegister extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		final IWXAPI api = WXAPIFactory.createWXAPI(context, null);
+		api.registerApp("wx61e52e13d5f5e847");
+	}
+}
